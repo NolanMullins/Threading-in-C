64d12dfd00c182120372876d90024da7480faabf
NolanMullins <nolan.mullins@me.com>
2017-01-30 15:18:07 -0500

    test folder for trying different things, currently has a fix for some warnings

--- src/main.c
+++ src/main.c
@@ -10,28 +10,30 @@ void *PrintHello(void *threadid)
    long tid;
    tid = (long)threadid;
    printf("Hello World! It's me, thread #%ld!\n", tid);
-   pthread_exit(NULL);
+   return NULL;
 }
 
 void *runfor(void *threadid)
 {
    clock_t start = clock(), diff;
    int msec = 0;
-   while (msec/1000 < 500)
+   while (msec/1000 < 50)
    {
       diff = clock() - start;
       msec = diff * 1000 / CLOCKS_PER_SEC;
    }
-   pthread_exit(NULL);
+   return NULL;
 }
 
 int main (int argc, char *argv[])
 {
-   pthread_t threads[NUM_THREADS];
+   int threadCount = sysconf(_SC_NPROCESSORS_ONLN);
+   pthread_t threads[threadCount];
    int rc;
    long t;
-   printf("%ld\n", sysconf(_SC_NPROCESSORS_ONLN));
-   for(t=0; t<NUM_THREADS; t++){
+   
+   printf("Available threads: %ld\n", sysconf(_SC_NPROCESSORS_ONLN));
+   for(t=0; t<threadCount; t++){
       printf("In main: creating thread %ld\n", t);
       rc = pthread_create(&threads[t], NULL, runfor, (void *)t);
       if (rc){
