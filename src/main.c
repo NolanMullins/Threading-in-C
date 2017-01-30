2088f8681d8d2bc193c7de1d52d8c602903c148c
NolanMullins <nolan.mullins@me.com>
2017-01-30 00:31:16 -0500

    space heater

--- src/main.c
+++ src/main.c
@@ -1,13 +1,26 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define NUM_THREADS     8
+#include <time.h>
+#define NUM_THREADS     7
 
 void *PrintHello(void *threadid)
 {
    long tid;
    tid = (long)threadid;
    printf("Hello World! It's me, thread #%ld!\n", tid);
+   pthread_exit(NULL);
+}
+
+void *runfor(void *threadid)
+{
+   clock_t start = clock(), diff;
+   int msec = 0;
+   while (msec/1000 < 500)
+   {
+      diff = clock() - start;
+      msec = diff * 1000 / CLOCKS_PER_SEC;
+   }
    pthread_exit(NULL);
 }
 
@@ -18,7 +31,7 @@ int main (int argc, char *argv[])
    long t;
    for(t=0; t<NUM_THREADS; t++){
       printf("In main: creating thread %ld\n", t);
-      rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+      rc = pthread_create(&threads[t], NULL, runfor, (void *)t);
       if (rc){
          printf("ERROR; return code from pthread_create() is %d\n", rc);
          exit(-1);
